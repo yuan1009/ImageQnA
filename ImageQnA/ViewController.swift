b77031def857ef8f8354f8e06992b206242ea067
ChenBingYuan <smallg1009@gmail.com>
2021-02-01 23:28:09 +0800

    Initial Commit

---
+++ ImageQnA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageQnA
+//
+//  Created by mac on 2021/2/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
